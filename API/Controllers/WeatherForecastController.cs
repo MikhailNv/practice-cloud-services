c04d653ae3df6d78960652c705d38936b922731f
PhilippKorkunov <71010445+PhilippKorkunov@users.noreply.github.com>
2023-12-25 20:03:26 +0300

    Update WeatherForecastController.cs

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy",
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm",
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
